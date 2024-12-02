eb4f00bce129fe56763323c5bd8c85f3e8aadcdc
조민제 <138378720+cmj9909@users.noreply.github.com>
2024-12-02 15:33:29 +0900

    학번: 202012499 이름: 조민제 - stack.c 수정

--- stack.c
+++ stack.c
@@ -9,6 +9,7 @@ Stack * create_stack(int initial_size)
 	s->contents = (int *)malloc(initial_size*sizeof(Item));
 	s->top=0;
 	s->size=initial_size;
+	 printf("Hello, World!\n"); // 코드 추가
 	return s;
 }
 
